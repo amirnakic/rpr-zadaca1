577512302fe553b7f72e8dbe0e11a6cbc03a3904
amirnakic <anakic1@etf.unsa.ba>
2018-11-06 00:49:41 +0100

    Pojednostavljena je metoda isQueensMoveCorrect.

--- src/ba/unsa/etf/rpr/Queen.java
+++ src/ba/unsa/etf/rpr/Queen.java
@@ -1,7 +1,5 @@
 package ba.unsa.etf.rpr;
 
-import java.util.ArrayList;
-
 public class Queen extends ChessPiece {
     public Queen(String position, ChessPiece.Color color) {
         super(position, color);
@@ -9,20 +7,11 @@ public class Queen extends ChessPiece {
 
     public boolean isQueensMoveCorrect(String position) {
         String currentPosition = getPosition();
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(5);
-        list.add(6);
-        list.add(7);
         int rezultat1 = Math.abs(currentPosition.charAt(0) - position.charAt(0));
         int rezultat2 = Math.abs((currentPosition.charAt(1) - '0') - (position.charAt(1) - '0'));
-        if (list.contains(rezultat1) && list.contains(rezultat2) && rezultat1 == rezultat2)
-            return true; //dijagonalno kretanje
-        else if (rezultat1 == 0 && list.contains(rezultat2)) return true; //vertikalno kretanje
-        else if (rezultat2 == 0 && list.contains(rezultat1)) return true; //horizontalno kretanje
+        if (rezultat1 == rezultat2) return true; //dijagonalno kretanje
+        else if (rezultat1 == 0 && rezultat2 != 0) return true; //vertikalno kretanje
+        else if (rezultat2 == 0 && rezultat1 != 0) return true; //horizontalno kretanje
         return false;
     }
 
