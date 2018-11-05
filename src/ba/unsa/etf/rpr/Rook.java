5f5b4c7e7022871001794c5d183831a2ff03d586
amirnakic <anakic1@etf.unsa.ba>
2018-11-06 00:29:25 +0100

    Unutar klase Rook implementirana je metoda isRooksMoveCorrect koja provjerava da li je traženi potez pomjeranja topa u skladu sa pravilima igre šah.

--- src/ba/unsa/etf/rpr/Rook.java
+++ src/ba/unsa/etf/rpr/Rook.java
@@ -1,8 +1,27 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Rook extends ChessPiece {
     public Rook(String position, ChessPiece.Color color) {
         super(position, color);
+    }
+
+    public boolean isRooksMoveCorrect(String position) {
+        String currentPosition = getPosition();
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
+        int rezultat1 = Math.abs(currentPosition.charAt(0) - position.charAt(0));
+        int rezultat2 = Math.abs((currentPosition.charAt(1) - '0') - (position.charAt(1) - '0'));
+        if (rezultat1 == 0 && list.contains(rezultat2)) return true; //vertikalno kretanje
+        else if (rezultat2 == 0 && list.contains(rezultat1)) return true; //horizontalno kretanje
+        return false;
     }
 
     @Override
