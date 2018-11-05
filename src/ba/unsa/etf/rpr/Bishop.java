c85a5fd4f3fe8f0f792e76317c1a5ddebed9ca55
amirnakic <anakic1@etf.unsa.ba>
2018-11-06 00:47:33 +0100

    Implementirana je metoda isBishopsMoveCorrect koja provjerava ispravnost pomjeranja lovca ovisno o pravilima igre šah.

--- src/ba/unsa/etf/rpr/Bishop.java
+++ src/ba/unsa/etf/rpr/Bishop.java
@@ -5,7 +5,16 @@ public class Bishop extends ChessPiece {
         super(position, color);
     }
 
+    public boolean isBishopsMoveCorrect(String position) {
+        String currentPosition = getPosition();
+        int rezultat1 = Math.abs(currentPosition.charAt(0) - position.charAt(0));
+        int rezultat2 = Math.abs((currentPosition.charAt(1) - '0') - (position.charAt(1) - '0'));
+        if (rezultat1 == rezultat2) return true;
+        return false;
+    }
+
     @Override
     public void move(String position) throws IllegalArgumentException, IllegalChessMoveException {
+        
     }
 }
