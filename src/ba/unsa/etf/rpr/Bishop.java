27f0ef614a283bcb74a627784d37bb7abf4539ee
amirnakic <anakic1@etf.unsa.ba>
2018-11-06 00:41:02 +0100

    Dodana je iz klase ChessPiece izvedena klasa Bishop, zajedno sa konstruktorom i prototipom preklopljene metode move.

---
+++ src/ba/unsa/etf/rpr/Bishop.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+
+public class Bishop extends ChessPiece {
+    public Bishop(String position, ChessPiece.Color color) {
+        super(position, color);
+    }
+
+    @Override
+    public void move(String position) throws IllegalArgumentException, IllegalChessMoveException {
+    }
+}
