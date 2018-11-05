ba46dcb35a7e6e48ece23357c70ca1cb16ae7dc1
amirnakic <anakic1@etf.unsa.ba>
2018-11-06 00:24:33 +0100

    Dodana je iz klase ChessPiece izvedena klasa Rook, zajedno sa njenim konstruktorom i prototipom preklopljene metode move.

---
+++ src/ba/unsa/etf/rpr/Rook.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr;
+
+public class Rook extends ChessPiece {
+    public Rook(String position, ChessPiece.Color color) {
+        super(position, color);
+    }
+
+    @Override
+    public void move(String position) throws IllegalArgumentException, IllegalChessMoveException {
+
+    }
+}
