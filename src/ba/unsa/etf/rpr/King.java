c322036199f590588f1a4359916ca814ea8c6817
amirnakic <anakic1@etf.unsa.ba>
2018-11-05 23:49:56 +0100

    Dodana je metoda isKingsMoveCorrect kojom provjeravamo da li je nova pozicija kralja realna u skladu sa pravilima igre šah.

--- src/ba/unsa/etf/rpr/King.java
+++ src/ba/unsa/etf/rpr/King.java
@@ -1,12 +1,26 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class King extends ChessPiece {
     public King(String position, ChessPiece.Color color) {
         super(position, color);
     }
 
+    public boolean isKingsMoveCorrect(String position) {
+        String currentPosition = this.getPosition();
+        ArrayList<Integer> list = new ArrayList<>(); //sadrži moguće razlike trenutne i nove pozicije za kralja
+        list.add(-1);
+        list.add(0);
+        list.add(1);
+        int rezultat1 = currentPosition.charAt(0) - position.charAt(0);
+        int rezultat2 = (currentPosition.charAt(1) - '0') - (position.charAt(1) - '0');
+        if (list.contains(rezultat1) && list.contains(rezultat2)) return true;
+        return false;
+    }
+
     @Override
     public void move(String position) throws IllegalArgumentException, IllegalChessMoveException {
-        
+
     }
 }
