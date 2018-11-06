7a02ce2d3006eab5b0dd9b2e0fc3b59bd68d76b0
amirnakic <anakic1@etf.unsa.ba>
2018-11-06 01:04:47 +0100

    Implementirana je metoda isKnightsMoveCorrect koja provjerava da li je potez pomjeranja konja u skladu sa pravilima igre šah.

--- src/ba/unsa/etf/rpr/Knight.java
+++ src/ba/unsa/etf/rpr/Knight.java
@@ -1,8 +1,21 @@
 package ba.unsa.etf.rpr;
 
+import java.util.ArrayList;
+
 public class Knight extends ChessPiece {
     public Knight(String position, ChessPiece.Color color) {
         super(position, color);
+    }
+
+    public boolean isKnightsMoveCorrect(String position) {
+        String currentPosition = getPosition();
+        ArrayList<Integer> lista = new ArrayList<>();
+        lista.add(1);
+        lista.add(2);
+        int rezultat1 = Math.abs(currentPosition.charAt(0) - position.charAt(0));
+        int rezultat2 = Math.abs((currentPosition.charAt(1) - '0') - (position.charAt(1) - '0'));
+        if (rezultat1 != rezultat2 && lista.contains(rezultat1) && lista.contains(rezultat2)) return true;
+        return false;
     }
 
     @Override
