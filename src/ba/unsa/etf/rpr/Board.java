7723a660cac3f0ff317705302fea87b5a23c6c8f
amirnakic <anakic1@etf.unsa.ba>
2018-11-08 02:07:08 +0100

    Reduciran broj privatnih atributa - ponovo sam sve aktivne figure pohranio u jednu zajedničku listu.

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -3,32 +3,31 @@ package ba.unsa.etf.rpr;
 import java.util.List;
 
 public class Board {
-    private List<ChessPiece> aktivneBijeleFigure;
-    private List<ChessPiece> aktivneCrneFigure;
+    private List<ChessPiece> aktivneFigure;
 
     public Board() {
-        aktivneBijeleFigure.add(new Rook("A1", ChessPiece.Color.WHITE));
-        aktivneBijeleFigure.add(new Rook("H1", ChessPiece.Color.WHITE));
-        aktivneCrneFigure.add(new Rook("A8", ChessPiece.Color.BLACK));
-        aktivneCrneFigure.add(new Rook("H8", ChessPiece.Color.BLACK));
-        aktivneBijeleFigure.add(new Knight("B1", ChessPiece.Color.WHITE));
-        aktivneBijeleFigure.add(new Knight("G1", ChessPiece.Color.WHITE));
-        aktivneCrneFigure.add(new Knight("B8", ChessPiece.Color.BLACK));
-        aktivneCrneFigure.add(new Knight("G8", ChessPiece.Color.BLACK));
-        aktivneBijeleFigure.add(new Bishop("C1", ChessPiece.Color.WHITE));
-        aktivneBijeleFigure.add(new Bishop("F1", ChessPiece.Color.WHITE));
-        aktivneCrneFigure.add(new Bishop("C8", ChessPiece.Color.BLACK));
-        aktivneCrneFigure.add(new Bishop("F8", ChessPiece.Color.BLACK));
-        aktivneBijeleFigure.add(new Queen("D1", ChessPiece.Color.WHITE));
-        aktivneCrneFigure.add(new Queen("D8", ChessPiece.Color.BLACK));
-        aktivneBijeleFigure.add(new King("E1", ChessPiece.Color.WHITE));
-        aktivneCrneFigure.add(new King("E8", ChessPiece.Color.BLACK));
+        aktivneFigure.add(new Rook("A1", ChessPiece.Color.WHITE));
+        aktivneFigure.add(new Rook("H1", ChessPiece.Color.WHITE));
+        aktivneFigure.add(new Rook("A8", ChessPiece.Color.BLACK));
+        aktivneFigure.add(new Rook("H8", ChessPiece.Color.BLACK));
+        aktivneFigure.add(new Knight("B1", ChessPiece.Color.WHITE));
+        aktivneFigure.add(new Knight("G1", ChessPiece.Color.WHITE));
+        aktivneFigure.add(new Knight("B8", ChessPiece.Color.BLACK));
+        aktivneFigure.add(new Knight("G8", ChessPiece.Color.BLACK));
+        aktivneFigure.add(new Bishop("C1", ChessPiece.Color.WHITE));
+        aktivneFigure.add(new Bishop("F1", ChessPiece.Color.WHITE));
+        aktivneFigure.add(new Bishop("C8", ChessPiece.Color.BLACK));
+        aktivneFigure.add(new Bishop("F8", ChessPiece.Color.BLACK));
+        aktivneFigure.add(new Queen("D1", ChessPiece.Color.WHITE));
+        aktivneFigure.add(new Queen("D8", ChessPiece.Color.BLACK));
+        aktivneFigure.add(new King("E1", ChessPiece.Color.WHITE));
+        aktivneFigure.add(new King("E8", ChessPiece.Color.BLACK));
         for (int i = 0; i < 8; i++) {
-            aktivneBijeleFigure.add(new Pawn(String.valueOf('A' + i) + "2", ChessPiece.Color.WHITE));
-            aktivneCrneFigure.add(new Pawn(String.valueOf('A' + i) + "7", ChessPiece.Color.BLACK));
+            aktivneFigure.add(new Pawn(String.valueOf('A' + i) + "2", ChessPiece.Color.WHITE));
+            aktivneFigure.add(new Pawn(String.valueOf('A' + i) + "7", ChessPiece.Color.BLACK));
         }
     }
 
-    public void move(Class type, ChessPiece.Color color, String position) throws IllegalChessMoveException {
+    public void move(Class type, ChessPiece.Color color, String position) throws IllegalArgumentException, IllegalChessMoveException {
     }
 }
