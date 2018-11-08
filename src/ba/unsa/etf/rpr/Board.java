de7b6d6c62c4c9700064feef777a06ad3c668ebc
amirnakic <anakic1@etf.unsa.ba>
2018-11-08 01:44:17 +0100

    Privatni atributi su razdvojeni po bojama figura, i dodan je konstruktor klase Board koji ploču postavlja u početno stanje.

--- src/ba/unsa/etf/rpr/Board.java
+++ src/ba/unsa/etf/rpr/Board.java
@@ -3,5 +3,29 @@ package ba.unsa.etf.rpr;
 import java.util.List;
 
 public class Board {
-    private List<ChessPiece> aktivneFigure;
+    private List<ChessPiece> aktivneBijeleFigure;
+    private List<ChessPiece> aktivneCrneFigure;
+
+    public Board() {
+        aktivneBijeleFigure.add(new Rook("A1", ChessPiece.Color.WHITE));
+        aktivneBijeleFigure.add(new Rook("H1", ChessPiece.Color.WHITE));
+        aktivneCrneFigure.add(new Rook("A8", ChessPiece.Color.BLACK));
+        aktivneCrneFigure.add(new Rook("H8", ChessPiece.Color.BLACK));
+        aktivneBijeleFigure.add(new Knight("B1", ChessPiece.Color.WHITE));
+        aktivneBijeleFigure.add(new Knight("G1", ChessPiece.Color.WHITE));
+        aktivneCrneFigure.add(new Knight("B8", ChessPiece.Color.BLACK));
+        aktivneCrneFigure.add(new Knight("G8", ChessPiece.Color.BLACK));
+        aktivneBijeleFigure.add(new Bishop("C1", ChessPiece.Color.WHITE));
+        aktivneBijeleFigure.add(new Bishop("F1", ChessPiece.Color.WHITE));
+        aktivneCrneFigure.add(new Bishop("C8", ChessPiece.Color.BLACK));
+        aktivneCrneFigure.add(new Bishop("F8", ChessPiece.Color.BLACK));
+        aktivneBijeleFigure.add(new Queen("D1", ChessPiece.Color.WHITE));
+        aktivneCrneFigure.add(new Queen("D8", ChessPiece.Color.BLACK));
+        aktivneBijeleFigure.add(new King("E1", ChessPiece.Color.WHITE));
+        aktivneCrneFigure.add(new King("E8", ChessPiece.Color.BLACK));
+        for (int i = 0; i < 8; i++) {
+            aktivneBijeleFigure.add(new Pawn(String.valueOf('A' + i) + "2", ChessPiece.Color.WHITE));
+            aktivneCrneFigure.add(new Pawn(String.valueOf('A' + i) + "7", ChessPiece.Color.BLACK));
+        }
+    }
 }
