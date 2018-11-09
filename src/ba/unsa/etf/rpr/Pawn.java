60bd7f7e75db8a35c59b62018d2c01bf780c4abe
amirnakic <anakic1@etf.unsa.ba>
2018-11-09 10:42:15 +0100

    Korekcija metode isPawnsMoveCorrect unutar klase Pawn.

--- src/ba/unsa/etf/rpr/Pawn.java
+++ src/ba/unsa/etf/rpr/Pawn.java
@@ -36,10 +36,17 @@ public class Pawn extends ChessPiece {
         position = position.toUpperCase();
         int result1 = currentPosition.charAt(0) - position.charAt(0);
         int result2 = (currentPosition.charAt(1) - '0') - (position.charAt(1) - '0');
-        if (result1 == 0 && result2 == -1)
-            return true; //iz bilo koje druge pozicije osim pocetne, pjeska se moze pomjerati samo za jedno mjesto iskljucivo prema naprijed
-        else if ((currentPosition.charAt(1) - '0') == 2 && result1 == 0 && result2 == -2)
-            return true; //iz pocetne pozicije pjesak moze da se pomjeri za dva mjesta iskljucivo prema naprijed
+        if (getColor() == Color.WHITE) {
+            if (result1 == 0 && result2 == -1)
+                return true; //iz bilo koje druge pozicije osim pocetne, pjeska se moze pomjerati samo za jedno mjesto iskljucivo prema naprijed
+            else if ((currentPosition.charAt(1) - '0') == 2 && result1 == 0 && result2 == -2)
+                return true; //iz pocetne pozicije pjesak moze da se pomjeri za dva mjesta iskljucivo prema naprijed
+        } else {
+            if (result1 == 0 && result2 == 1)
+                return true;
+            else if ((currentPosition.charAt(1) - '0') == 7 && result1 == 0 && result2 == 2)
+                return true;
+        }
         return false;
     }
 
