f41582805b11df25e66b40a1036a51d2770f7ada
amirnakic <anakic1@etf.unsa.ba>
2018-11-10 19:07:27 +0100

    Dodani su testovi za metode move i eat klase Pawn, u slučajevima kada trebaju i ne trebaju da bace izuzetke.

--- test/ba/unsa/etf/rpr/PawnTest.java
+++ test/ba/unsa/etf/rpr/PawnTest.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PawnTest {
@@ -12,4 +14,21 @@ class PawnTest {
         );
     }
 
+    @Test
+    void move2() {
+        Pawn p = new Pawn("E5", ChessPiece.Color.BLACK);
+        assertThrows(IllegalChessMoveException.class, () -> p.move("E6"));
+    }
+
+    @Test
+    void eat1() {
+        Pawn p = new Pawn("d2", ChessPiece.Color.WHITE);
+        assertDoesNotThrow(() -> p.eat("e3"));
+    }
+
+    @Test
+    void eat2() {
+        Pawn p = new Pawn("d7", ChessPiece.Color.BLACK);
+        assertThrows(IllegalChessMoveException.class, () -> p.eat("F5"));
+    }
 }
